52ca842e635fe3a165bebd2587958add00aeb3cb
Johannes Foulds <johannes.foulds@vodacom.co.za>
2022-07-09 16:45:04 +0200

    Minor spelling

--- test_sha256.py
+++ test_sha256.py
@@ -7,14 +7,14 @@ from Library.opencl_information import opencl_information
 from binascii import hexlify
 
 
-def sha256_test(opencl_algo, passwordlist):
+def sha256_test(opencl_algo, password_list):
     print("Testing sha256 ..")
     ctx = opencl_algo.cl_sha256_init()
-    clresult = opencl_algo.cl_sha256(ctx, passwordlist)
+    cl_result = opencl_algo.cl_sha256(ctx, password_list)
     
-    for i, pwd in enumerate(passwordlist):
+    for i, pwd in enumerate(password_list):
         print("Password: %s" % pwd)
-        print("CL result      : %s" % hexlify(clresult[i]))
+        print("CL result      : %s" % hexlify(cl_result[i]))
         print("Correct result : %s" % hexlify(hashlib.sha256(pwd).digest()))
         print("")
 
@@ -29,14 +29,14 @@ def main(argv):
         return
 
     # Input values to be hashed
-    passwordlist = [b'password', b'hmm', b'trolololl', b'madness']
+    password_list = [b'password', b'madness']
 
     platform = int(argv[1])
     debug = 0
-    write_combined_file = False
+    write_combined_file = True
     opencl_algos = opencl.opencl_algos(platform, debug, write_combined_file, inv_memory_density=1)
 
-    sha256_test(opencl_algos, passwordlist)
+    sha256_test(opencl_algos, password_list)
 
     print("Tests have finished.")
 
